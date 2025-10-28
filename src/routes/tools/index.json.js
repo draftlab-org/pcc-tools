30c9131cec3567689ee9d3beb11fc387085fb399
Tin Geber <tingeber@gmail.com>
2025-10-28 09:45:13 +0100

    change API URL to draftlab dev, remove husky

--- src/routes/tools/index.json.js
+++ src/routes/tools/index.json.js
@@ -1,21 +1,23 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+import appConfig from "../../_helpers/app-config";
 
-import processTools from '../_helpers/process-tools';
-import sortByObjectKey from '../_helpers/sort-by-object-key';
+import processTools from "../_helpers/process-tools";
+import sortByObjectKey from "../_helpers/sort-by-object-key";
 
 export async function get(req, res, next) {
 	let tools = [];
 
-	await fetch('https://directory.platform.coop/api/tools/')
-		.then(result => result.json())
-		.then(json => {
-			tools = json.map(processTools).sort(function(a, b) {
+	await fetch(appConfig.directoryUrls.toolsApi)
+		.then((result) => result.json())
+		.then((json) => {
+			console.log(json[0]);
+			tools = json.map(processTools).sort(function (a, b) {
 				return sortByObjectKey(a, b, "name");
 			});
 		});
 
 	if (tools !== []) {
-		res.setHeader('Content-Type', 'application/json');
+		res.setHeader("Content-Type", "application/json");
 		res.end(JSON.stringify(tools));
 	} else {
 		next();
